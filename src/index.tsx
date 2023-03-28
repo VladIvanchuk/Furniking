cbbf0343bfa9c10a0d7496bc4bf8bd74f9fd0c2d
VladIvanchuk <gidra465@gmail.com>
2023-03-28 15:15:02 +0300

    setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./assets/scss/index.scss";
+import "modern-normalize";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
