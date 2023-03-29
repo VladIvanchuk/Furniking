6efce0d89d1242ade5bdae3c3d814521888f73f5
AndreyChernysh <achernysh744@gmail.com>
2023-03-29 14:27:50 +0300

    add some components & base routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./assets/scss/index.scss";
 import "modern-normalize";
 import { BrowserRouter } from "react-router-dom";
+import Routers from "./routes/Routers";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routers />
     </BrowserRouter>
   </React.StrictMode>
 );
