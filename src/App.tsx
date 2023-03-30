2cbf745c60c327af35527f5cdbc45d2b37869322
AndreyChernysh <achernysh744@gmail.com>
2023-03-30 16:16:30 +0300

    fix header & make hero section & add Continer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { Header, Footer } from "./components";
 import Router from "./router/Router";
-import s from "./App.module.scss";
 
 const App = () => {
   return (
     <>
       <Header />
-      <div className={s.body_container}>
         <Router />
-      </div>
       <Footer />
     </>
   );
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,8 +1,32 @@
+import s from "./Hero.module.scss";
+import Slider from "../UI/Slider/Slider";
+import Container from "../Container/Container";
+
+const allCollection : Array<string> = [
+  "All",
+  "New arrivals",
+  "Hot Sale",
+  "Furniture",
+  "Amrature",
+  "Tabble",
+  "Chair",
+  "Mirrors",
+  "Sofa"
+]
 const Hero = () => {
   return (
-    <div>
-      Hero
-    </div>
+    <Container>
+      <section className={s.hero}>
+        <ul className={s.hero__collection}>
+          {allCollection.map((category: string, id : number) => (
+            <li key={id} className={s.hero__option}>
+              <button type={"button"} className={s.hero__category}>{category}</button>
+            </li>
+          ))}
+        </ul>
+        <Slider />
+      </section>
+    </Container>
   );
 };
 
