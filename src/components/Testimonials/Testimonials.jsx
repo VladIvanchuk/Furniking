8af524c1be9572efc9d7ea368a73e51853ab104f
Anton <antonkekdurachek@gmail.com>
2023-04-01 21:21:13 +0300

    adding users reply section

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,9 +1,10 @@
 import s from './Testimonials..module.scss';
-
+import TestimonialSlider from './TestimonialsSlider/TestimonialsSlider'
 const Testimonials = () => {
   return (
     <div className={s.testimonials}>
       <h1>What Our Customer Says</h1>
+      <TestimonialSlider/>
     </div>
   );
 };
