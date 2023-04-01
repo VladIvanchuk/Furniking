885c20fe53f9850be663e1d9475a848f11a62936
AndreyChernysh <achernysh744@gmail.com>
2023-04-01 17:16:10 +0300

    section Our product & change font of trending title

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,13 @@
-import { Hero, Offer, Trending } from "../../components";
+import { Hero, Offer } from "../../components";
+import Trending from "../../components/Trending/Trending";
+import Product from "../../components/Product/Product";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Trending />
+      <Product />
       <Offer />
     </>
   );
